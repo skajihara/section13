4b338788f4de0adcd16c4c817d853635cdc4f7d9
skajihara <t15cs015@gmail.com>
2024-04-01 17:54:57 +0900

    section13-4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup></script>
 <template>
   <h1>Vue Router</h1>
-  <RouterLink to="/">Home</RouterLink> | <RouterLink to="/about">About</RouterLink>
+  <RouterLink :to="'/?lang=ja#title'" replace>Home</RouterLink> |
+  <button @click="$router.push({ path: '/about' })">About</button>
   <RouterView />
 </template>
